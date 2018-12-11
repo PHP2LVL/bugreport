2c732928950c9cdcee56351ada517f35f3038bfb
tomejus9 <tomejus9@gmail.com>
2018-12-11 20:30:30 +0200

    dashboard reverse

--- dieva/pages/dashboard.php
+++ dieva/pages/dashboard.php
@@ -21,9 +21,61 @@ $progresas = procentai( ( !empty( $stats['uzvakar'] ) ? $stats['uzvakar'] : 1 ),
 $data2 = [];
 $res   = mysql_query1( "SELECT * FROM `" . LENTELES_PRIESAGA . "page` WHERE `lang`=" . escape( lang() ) . " ORDER BY `place` ASC" );
 foreach($res as $row) {
-	if (teises($row['teises'], $_SESSION[SLAPTAS]['level'])) {
-		$data2[$row['parent']][] = $row;
-	}
+    if (teises($row['teises'], $_SESSION[SLAPTAS]['level'])) {
+        $data2[$row['parent']][] = $row;
+    }
 }
 ?>
 
+<div class="row clearfix">
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <?php infoBox('green', 'perm_identity', $lang['online']['users'] , (int)$sql['svec']); ?>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <?php infoBox('orange', 'face', $lang['online']['registeredmembers'], (int)$sql['useriai']); ?>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <?php infoBox('blue', 'person_add', $lang['online']['today'], $stats['siandien']); ?>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <?php infoBox('red', 'devices', $lang['online']['traffic_in'], $progresas . '%'); ?>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        <?php dashTree($lang['system']['tree'], build_tree($data2)); ?>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        <div class="card">
+            <div class="header bg-cyan">
+                <h2>
+                    Mightmedia <?php echo $lang['news']['news']; ?>
+                </h2>
+            </div>
+            <div class="body">
+                <div class="list-group">
+                    <?php
+                    $feedUrl = 'https://mightmedia.lt/rss.php';
+                    $rssContent = getFeedArray($feedUrl);
+                    $i = 0;
+                    ?>
+                    <?php foreach($rssContent->item as $key => $new) { ?>
+                        <?php
+                        ++$i;
+                        $date = strtotime($new->pubDate[0]);
+                        $date = date('Y-m-d', $date);
+                        ?>
+                        <a href="<?php echo $new->link->__toString() ; ?>" class="list-group-item">
+                            <?php echo $new->title->__toString(); ?>
+                            <span class="badge<?php echo ($i == 1 ? ' bg-green' : ''); ?>"><?php echo $date; ?></span>
+                        </a>
+                        <?php
+                        if ($i > 5) {
+                            break;
+                        }
+                        ?>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+        <?php dashStats($lang['system']['visits'], $stats); ?>
+    </div>
+</div>
